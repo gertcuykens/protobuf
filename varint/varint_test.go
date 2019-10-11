ba250a7942e0f96e2582f08b6bc4b099d54f4356
Gert Cuykens <gert.cuykens@gmail.com>
2019-10-11 04:31:28 +0200

    varint

--- varint/varint_test.go
+++ varint/varint_test.go
@@ -3,16 +3,19 @@ package varint
 import (
 	"encoding/binary"
 	"fmt"
-	"testing"
 )
 
-func TestVarInt(t *testing.T) {
-	buf := make([]byte, binary.MaxVarintLen64)
-	binary.PutUvarint(buf, 300)
-	fmt.Printf("300 % x \n", buf)
-	b := []byte{0x0a, 0x04, 0x74, 0x65, 0x73, 0x74, 0x10, 0x00}
-	b = []byte{0xac, 0x02, 0x00, 0x00}
-	// b = []byte{0x08, 0x00}
-	x, n := binary.Uvarint(b)
-	fmt.Printf("%d % x %d \n", x, b, n)
+func ExampleVarInt() {
+	var d uint64 = 300
+	x := make([]byte, binary.MaxVarintLen64)
+	n := binary.PutUvarint(x, 300)
+	fmt.Printf("%d / % x / %d\n", d, x[:n], n)
+	// x = []byte{0x0a, 0x04, 0x74, 0x65, 0x73, 0x74, 0x10, 0x00}
+	// x = []byte{0x08, 0x00}
+	// x = []byte{0xac, 0x02, 0x00, 0x00}
+	d, n = binary.Uvarint(x[:n])
+	fmt.Printf("%d / % x / %d\n", d, x[:n], n)
+	// Output:
+	// 300 / ac 02 / 2
+	// 300 / ac 02 / 2
 }
